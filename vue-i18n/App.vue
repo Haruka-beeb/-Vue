48dd5c940486f296d0d71af9efd66fb08d89dfa5
Haruka <anastasiapetrova1231@gmail.com>
2025-04-23 01:42:13 +0800

    Create App.vue

---
+++ vue-i18n/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <WeatherInfo />
+  </div>
+</template>
+
+<script>
+import WeatherInfo from './components/WeatherInfo.vue';
+
+export default {
+  components: {
+    WeatherInfo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
